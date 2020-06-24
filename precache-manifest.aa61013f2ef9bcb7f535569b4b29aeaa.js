515f5cac23880f5f347f6bfb9b517386ba2ec34a
Karim Gehad <44138860+MiraK1@users.noreply.github.com>
2020-06-24 19:12:58 +0100

    first commit

---
+++ precache-manifest.aa61013f2ef9bcb7f535569b4b29aeaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df62cd9f069703a39e1bf35600d2866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7d707b2a7da189c7d77",
+    "url": "/static/css/main.95dd4833.chunk.css"
+  },
+  {
+    "revision": "373b3a905073eb840d7e",
+    "url": "/static/js/2.ec859118.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.ec859118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7d707b2a7da189c7d77",
+    "url": "/static/js/main.91ca53f9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
